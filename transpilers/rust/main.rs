061c4e0dbef036c5b03ef35e8bf763aec29b221b
Aurorans Solis <primalucegd@gmail.com>
2020-09-22 13:00:53 -0700

    New test runtime.

--- transpilers/rust/main.rs
+++ transpilers/rust/main.rs
@@ -1,20 +1,40 @@
 use logos::Logos;
-use mips_parser::Token;
+use mips_parser::{Error as LexParseError, Parser};
+use std::{
+    fs::{read_to_string, write},
+    io::{Error as IoError, Read, Write},
+    path::Path,
+};
+use structopt::StructOpt;
 
-const TEST: &str = r#".data
-.globl main
-.text
- 
-main:
-	addi $s0, $zero, 20 # Initialize $s0 to 20
-	addi $s1, $zero, 30 # Initialize $s1 to 30
-	addi $s2, $zero, -2 # Initialize $s2 to -2"#;
-    
-fn main() {
-    let mut lex = Token::lexer(TEST);
-    while let Some(token) = lex.next() {
-        if token != Token::Error {
-            println!("token: {:?}", token);
-        }
+#[derive(StructOpt)]
+#[structopt(name = "m2rs", version = "0.0.1", author = "Aurorans Solis")]
+struct Arguments {
+    #[structopt(short = "i", long = "input")]
+    input_file: String,
+    #[structopt(short = "o", long = "output")]
+    output_file: Option<String>,
+    #[structopt(short = "f", long = "force")]
+    force: bool,
+}
+
+#[derive(Debug)]
+enum Error {
+    LexParseError(LexParseError),
+    IoError(IoError),
+}
+
+fn main() -> Result<(), Error> {
+    let Arguments {
+        input_file,
+        output_file,
+        force,
+    } = Arguments::from_args();
+    let file_contents = read_to_string(&input_file).map_err(|e| Error::IoError(e))?;
+    let parser = Parser::new(&file_contents);
+    let expressions = parser.parse().map_err(|e| Error::LexParseError(e))?;
+    for expression in expressions {
+        println!("expression: {:?}", expression);
     }
+    Ok(())
 }
